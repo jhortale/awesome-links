24e479a5f107defb18642d0f49de9da3f2f932cf
Joao Hortale <jhortale@gmail.com>
2021-10-04 15:12:00 -0300

    pagination

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
+import { ApolloProvider } from '@apollo/client'
+import apolloClient from '../lib/apollo'
 import '../styles/tailwind.css';
 import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ApolloProvider client={apolloClient}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ApolloProvider>
   );
 }
 
